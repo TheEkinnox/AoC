ba8894b717ecba155477aec09e13523b497f74e4
Loick Noa Obiang Ndong <l.obiangndong@student.isartdigital.com>
2024-12-01 23:47:15 -0500

    Completed day 01 - Dec 2024

---
+++ 2024/01/day01.py
@@ -0,0 +1,27 @@
+﻿from utility import read_columns
+
+
+def step1( columns ):
+    for col in columns:
+        col.sort()
+
+    dist_sum = 0
+    for i in range( 0, len( columns[ 0 ] ) ):
+        dist_sum += abs( columns[ 0 ][ i ] - columns[ 1 ][ i ] )
+
+    print( "Sum: " + str( dist_sum ) )
+
+
+def step2( columns ):
+    sim_sum = 0
+
+    for val in columns[ 0 ]:
+        sim_sum += val * columns[ 1 ].count( val )
+
+    print( "Similarity: " + str( sim_sum ) )
+
+
+cols = read_columns( "input.txt", 2, ' ' )
+
+step1( cols )
+step2( cols )
---
+++ 2024/utility/__init__.py
@@ -0,0 +1,17 @@
+﻿def read_columns( path: str, count: int, separator: str ):
+    columns = [ ]
+
+    for i in range( 0, count ):
+        columns.append( [ ] )
+
+    with open( path, 'r' ) as file:
+        for line in file:
+            if len( line ) == 0:
+                return
+
+            tokens = line.split( separator, count - 1 )
+
+            for i in range( 0, count ):
+                columns[ i ].append( int( tokens[ i ] ) )
+
+    return columns
